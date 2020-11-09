cd2bb292ab662272a27f6a9bdb1cffd376d3f1c3
Averi Bates <Averi.J.Bates@ou.edu>
2020-11-09 16:58:39 -0600

    Last Commit

--- src/FileASize.java
+++ src/FileASize.java
@@ -1,10 +1,25 @@
 import java.io.File;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 
 public class FileASize {
 	public static long getSize(File file) {
 		long size = 0;
+		if (file.isDirectory())
+			{
+				File[] files = file.listFiles();
+				List<File> xFiles = new LinkedList<>(Arrays.toList(files));
+				for(File f : xFiles)
+				{
+					size +=getSize(f);
+				}
+			}
+		else {
+		size += file.length();
+		}
 		return size;
 	}
 	public static void main(String[] args)
