8276a5020d0ca66fc244ff8bfcd1ff7f2a20de90
Averi Bates <Averi.J.Bates@ou.edu>
2020-11-09 16:12:22 -0600

    Creation of takeABite

--- src/Recursion.java
+++ src/Recursion.java
@@ -1,4 +1,19 @@
 
 public class Recursion {
+	private int bites = 10;
+			
+	public Recursion(int bites)
+	{
+		this.bites = bites;
+	}
+	public Recursion()
+	{
+		//fix me
+	}
+	
+	public void takeABite()
+	{
+		
+	}
 
 }
